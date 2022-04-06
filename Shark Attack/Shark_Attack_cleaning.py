bae6bdcdaa7f92bded5c2e6d32daf477da346344
Thaisdahack <thaishelenaocean@gmail.com>
2022-04-06 09:37:01 -0300

    Update Shark_Attack_cleaning.py
    Update of dataset whith documentation

--- Shark Attack/Shark_Attack_cleaning.py
+++ Shark Attack/Shark_Attack_cleaning.py
@@ -10,7 +10,7 @@ pwd
 # In[ ]:
 
 
-#importando as bibliotecas
+#importing libraries
 import pandas as pd
 import numpy as np
 import re
@@ -19,10 +19,10 @@ import re
 # In[ ]:
 
 
-#importando o arquivo
+#importing files
 shark = pd.read_csv('attacks.csv')
 
-#verificando a estrutura do dataset
+#checking dataset structure
 shark.head(5)
 shark.shape #25723 linhas, 24 colunas
 shark.size
@@ -32,9 +32,12 @@ shark.size
 
 
 
-shark.columns #chamando as colunas
+shark.columns #checking columns names
 
 
+# 
+# Questions to be answered:
+# 
 # Question 1: Which hemisphere has more ocurrences?
 # Question 2: Which hemisphere has more fatalities?
 # Question 3: Which activity has more fatalities?
@@ -49,7 +52,7 @@ shark.columns #chamando as colunas
 # In[5]:
 
 
-#verificando valores nulos
+#checking null values on dataframe
 import seaborn as sns
 sns.set({'figure.figsize':(20,20)})
 sns.heatmap(shark.isnull(),cbar=False)
@@ -58,7 +61,7 @@ sns.heatmap(shark.isnull(),cbar=False)
 # In[6]:
 
 
-shark.info() #country tem mais de 6200 nulos/ year 6300/ atividade 5758 de um total de 25725
+shark.info() #country has more than 6200 nulls/ year 6300/ activity 5758 out of a total of 25725
 
 
 # In[7]:
@@ -76,11 +79,12 @@ shark.head(10)
 # In[37]:
 
 
-#ecnontrando valores duplicados
+
+#finding duplicate values
 shark[shark.duplicated(keep='first')]
 
 
-#apagando valores duplicados 
+#deleting duplicate values
 
 shark.drop_duplicates(subset=None, keep='first', inplace=True)
 
@@ -100,7 +104,7 @@ shark.head(5)
 # In[40]:
 
 
-#selecionando um subset do dataframe (series)
+#selecting  subsets of the dataframe (series)
 shark.columns
 new_shark = shark[["Country", "Fatal (Y/N)", "Activity", "Area"]]
 
@@ -110,7 +114,7 @@ new_shark
 # In[13]:
 
 
-#verificando valores nulos do novo dataset/ tirei o ano
+#checking null values of new dataset/ with columns: 'Country', 'Area', 'Activity', 'Fatal'
 import seaborn as sns
 sns.set({'figure.figsize':(20,20)})
 sns.heatmap(new_shark.isnull(),cbar=False)
@@ -138,7 +142,7 @@ new_shark.head(10)
 # In[17]:
 
 
-# verificando a porcentagem de linhas preechidas no dataset
+# checking the percentage of rows filled in the dataset
 new_shark["Country"].isnull().value_counts(normalize=True) #99% dos paises esta preenchido
 new_shark["Fatal (Y/N)"].isnull().value_counts(normalize=True) #91% esta preenchido
 new_shark["Area"].isnull().value_counts(normalize=True) #92% preenchido
@@ -148,10 +152,10 @@ new_shark["Activity"].isnull().value_counts(normalize=True)#91% preenchido
 # In[41]:
 
 
-#removendo caracteres especiais
+#removing special characters
 new_shark.replace('\W',' ',regex=True)
 
-#retirando as casas decimais da coluna ano
+#removing the decimal places from the year column
 #pd.options.display.float_format = '{:.0f}'.format
 
 
@@ -167,15 +171,15 @@ new_shark.head(30)
 # In[42]:
 
 
-# contar valores unicos paises
+# count unique country values
 
 new_shark.Country.unique
 
 
 shark_top = new_shark.Country.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
 shark_top
-#Eua onde tem maior frequencia de ataques
-shark_mask= shark_top>100 #mascara para pegar valores unicos de paises maior que 100 ocorrencias
+#USA where has the highest frequency of attacks
+shark_mask= shark_top>100 #mask to get unique values from countries greater than 100 occurrences
 shark_top.loc[shark_mask].index
 list_top= shark_top.loc[shark_mask].index
 
@@ -187,21 +191,20 @@ list_top= shark_top.loc[shark_mask].index
 # In[43]:
 
 
-mask_top = new_shark['Country'].isin(list_top) #isso é uma mascara para verificar se list_top esta dentro da coluna country
-
-new_shark =new_shark[mask_top] #aplicando a mascara no dataframe
+mask_top = new_shark['Country'].isin(list_top) # mask to check if list_top is inside the country column
+new_shark =new_shark[mask_top] #applying the mask on the dataframe
 
 
 # In[44]:
 
 
-shark_top.loc[shark_mask].index #paises com mais de 100 ocorrencias
+shark_top.loc[shark_mask].index #countries with more than 100 occurrences
 
 
 # In[46]:
 
 
-#ADICIONANDO OUTRA COLUNA COM OS HEMISFERIOS
+#ADDING ANOTHER COLUMN WITH THE HEMISPHERES N and S
 new_shark.loc[(new_shark['Country'] == 'USA') | (new_shark['Country'] == 'BAHAMAS'), 'Hemisphere'] = 'North'
 new_shark
 new_shark.loc[(new_shark['Country'] == 'AUSTRALIA') | (new_shark['Country'] == 'SOUTH AFRICA') |  (new_shark['Country'] == 'NEW ZELAND') |  (new_shark['Country'] == 'BRAZIL') | (new_shark['Country'] == 'PAPUA NEW GUINEA'), 'Hemisphere'] = 'South'
@@ -211,7 +214,7 @@ new_shark
 # In[47]:
 
 
-#contando valores unicos de cada hemisferio
+#counting unique values from each hemisphere
 count_hem = new_shark.Hemisphere.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True) #North    2338 / South    2163
 count_hem
 
@@ -221,14 +224,14 @@ count_hem
 # In[48]:
 
 
-#contando valores unicos de cada atividade #surfing tem mais ocorrencias
+#counting unique values of each activity #surfing has more occurrences
 new_shark.Activity.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
 
 
 # In[49]:
 
 
-#renomeando a coluna "Fatal (Y/N) para Fatal"
+#renaming column "Fatal (Y/N) to Fatal"
 new_shark.columns
 new_shark.rename(columns={'Fatal (Y/N)': 'Fatal'}, inplace=True)
                        
@@ -238,14 +241,15 @@ new_shark
 # In[50]:
 
 
-#quantos incidentes fatais ocorreram? 3488 nao fatais e 703  fatais
+#how many fatal incidents occurred? 
+#3488 non-fatal and 703 fatal
 
 #new_shark['Fatal'] = new_shark.loc[:,'Fatal'].str.strip()
 import re
 new_shark['Fatal'] = new_shark['Fatal'].str.replace('\s?N\s?','N') #usando regex (\s? pode ter espaço ou nao N(padrao) \s? pode ter espaço depois ou nao)
 new_shark
 
-#quantos incidentes fatais ocorreram? 3488 nao fatais e 703  fatais
+
 new_shark.Fatal.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
 
 
@@ -264,7 +268,7 @@ new_shark.Fatal.value_counts(normalize=False, sort=True, ascending=False, bins=N
 # In[51]:
 
 
-# verificando qual hemisferio tem mais ataques fatais
+# checking which hemisphere has the most fatal attacks
 
 new_shark.groupby(by=['Hemisphere', 'Fatal']).count()[['Country']] # agrupou o hemisferio e o fatal como indices e contou pelo numero de paises que seria as ocorrencias/ colocar 2 parenteses para ficar com cara de parenteses 
 
@@ -275,6 +279,7 @@ new_shark.groupby(by=['Hemisphere', 'Fatal']).count()[['Country']] # agrupou o h
 # In[131]:
 
 
+#Fazer uma funçao aqui
 fatal_total=703
 fatal_south= 483
 (fatal_south*100)/fatal_total
@@ -290,15 +295,16 @@ fatal_south= 483
 # In[132]:
 
 
-
+#checking the activity with the most fatalities
 new_shark.Activity.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
-#A atividade com mais fatalidade é a nataçao
+#The most fatal activity is swimming
 new_shark.groupby(by=["Activity", "Fatal"]).count()[['Country']].sort_values("Country",ascending=False)
 
 
 # In[134]:
 
 
+#checking which activity was most practiced during the occurrences
 new_shark.Activity.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
 
 
@@ -307,21 +313,21 @@ new_shark.Activity.value_counts(normalize=False, sort=True, ascending=False, bin
 
 fatal_total=703
 fatal_swimming= 172
-(fatal_swimming*100)/fatal_total #24% foi fatal na natacao
+(fatal_swimming*100)/fatal_total #24% were fatal in swimming
 
 
 # In[137]:
 
 
-nao_fatal=3488
-(nao_fatal*100)/total_ocurrences
-#75% das ocorrencias sao nao fatais
+not_fatal=3488
+(not_fatal*100)/total_ocurrences
+#75% of incidents are non-fatal
 
 
 # In[54]:
 
 
-# Distinguindo correntes de contorno Oeste
+#Distinguishing western boundary currents
 
 new_shark.head(10)
 
@@ -331,7 +337,7 @@ new_shark.Area.value_counts(normalize=False, sort=True, ascending=False, bins=No
 # In[91]:
 
 
-#colocando os estados dos EUA no corrente de contorno oeste(estados do leste do EUA)
+#putting the eastern states of the US that are on the route of the western boundary currents
 new_shark.loc[new_shark['Area'].isin (['Connecticut','Delaware','Florida','Georgia','Maryland','New Hampshire','North Carolina','Pennsylvania','Rhode Island','South Carolina']), "Current"]="Warm Current"
 new_shark
 
@@ -339,14 +345,14 @@ new_shark
 # In[32]:
 
 
-#usei para colocar aspas em todas as palavras
+#I used to put quotes around all the words
 "Connecticut, Delaware, Florida, Georgia, Maryland, New Hampshire, North Carolina, Pennsylvania, Rhode Island, South Carolina".split(", ")
 
 
 # In[56]:
 
 
-#colocando os estados da australia no corrente de contorno oeste(estados do leste da Australia)
+#placing the Australian east coast states that are in the route of the western boundary current
 new_shark.loc[new_shark['Area'].isin (['Victoria', 'New South Wales', 'Queensland']), "Current"]="Warm Current"
 
 new_shark       
@@ -355,13 +361,13 @@ new_shark
 # In[34]:
 
 
-# Paises que estao na tabela: Index(['USA', 'AUSTRALIA', 'SOUTH AFRICA', 'PAPUA NEW GUINEA', 'NEW ZEALAND', 'BRAZIL', 'BAHAMAS']
+# Countrys in dataframe: Index(['USA', 'AUSTRALIA', 'SOUTH AFRICA', 'PAPUA NEW GUINEA', 'NEW ZEALAND', 'BRAZIL', 'BAHAMAS']
 
 
 # In[57]:
 
 
-#setando o indice para o Country ser o novo indice
+#setting the dataframe index for the "Country" column to be the new index and it will be possible to make the groupings
 new_shark.set_index(['Country'], inplace = True)
 new_shark
 
@@ -375,7 +381,7 @@ new_shark.loc['SOUTH AFRICA']
 # In[92]:
 
 
-#mudando as correntes para Africa do Sul
+#placing the South Africa east coast states that are in the route of the western boundary current
 new_shark.loc[new_shark['Area'].isin (['Eastern Cape Province', 'Western Cape Province', 'KwaZulu-Nata']), "Current"]="Warm Current"
 
 new_shark  
@@ -384,14 +390,14 @@ new_shark
 # In[101]:
 
 
-##mudando as correntes para New Zealand
+##placing the New Zealand east coast states that are in the route of the western boundary current
 new_shark.loc[new_shark['Area'].isin (['North Island']), 'Current']="Warm Current"
 
 
 # In[102]:
 
 
-#substituindo valores de brazil na coluna Current para warm current
+#splacing the Brazilian east coast states that are in the route of the western boundary current
 
 new_shark.loc['BRAZIL', 'Current'] ="Warm Current"
 new_shark.head(60)
@@ -400,6 +406,7 @@ new_shark.head(60)
 # In[94]:
 
 
+#placing the Bahamas  coast that are in the route of the western boundary current
 new_shark.loc['BAHAMAS', 'Current'] ="Warm Current"
 
 
@@ -412,7 +419,7 @@ new_shark.head(50)
 # In[ ]:
 
 
-#esses sao os paises ->Index(['USA', 'AUSTRALIA', 'SOUTH AFRICA', 'PAPUA NEW GUINEA', 'NEW ZEALAND','BRAZIL', 'BAHAMAS'])
+#the countries ->Index(['USA', 'AUSTRALIA', 'SOUTH AFRICA', 'PAPUA NEW GUINEA', 'NEW ZEALAND','BRAZIL', 'BAHAMAS'])
 
 
 # In[ ]:
@@ -424,7 +431,7 @@ new_shark.head(50)
 # In[80]:
 
 
-#dropar a coluna current
+#droping current column
 new_shark = new_shark.drop(columns=["current"]) 
 
 
@@ -437,21 +444,22 @@ new_shark.head(60)
 # In[122]:
 
 
-#Contand valores para Warm Current -> 2871 locais estao em areas de contorno oeste
+#Counting values whith Warm Currents -> 2871 areas are in the western boundary current
 total_warm_current = new_shark.Current.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
 
 
 # In[123]:
 
 
-#Agrupando correntes quentes por paises
-#new_shark.groupby(by=['Current']).count()[['Country']]
+#Grouping warm currents by hemisphere counting by countries
+
 new_shark.groupby(by=['Current', 'Hemisphere']).count()[['Country']]
 
 
 # In[135]:
 
 
+#chcking the total of ocurrences
 total_ocurrences= len(new_shark)
 total_ocurrences
 
@@ -459,7 +467,7 @@ total_ocurrences
 # In[126]:
 
 
-#62% das ocorrencias ocorreram em locais com corrente de contorno oeste
+#62% of occurrences in locations with a western boundary current
 (total_warm_current*100)/total_ocurrences
 
 
